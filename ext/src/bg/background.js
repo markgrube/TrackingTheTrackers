f4b94816f96fb94aa25c67a7f34b44cd1ffec2a2
Mark Grube <markg140@gmail.com>
2017-11-04 15:04:20 -0700

    added local storage support for logging

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -11,3 +11,22 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+function addToDb(host, tracker){
+    addPairToDb(host, tracker);
+    addPairToDb(tracker, host);
+}
+
+function addPairToDb(string1, string2){
+    var obj = {};
+    obj[string1] = [];
+    chrome.storage.local.get(string1, function(result){
+        if (result[string1] !== undefined) {
+            obj[string1] = result[string1];
+        }
+        if (obj[string1].indexOf(string2) === -1) {
+            obj[string1].push(string2)
+        }
+        chrome.storage.local.set(obj);
+    });
+}
