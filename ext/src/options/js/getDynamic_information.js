b578005c14bf6db80db909e60a29abc775906a3d
david-agumya <agumya.d@husky.neu.edu>
2017-11-04 16:31:32 -0700

    re arrange

---
+++ ext/src/options/js/getDynamic_information.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dagumya on 11/4/17.
+ */
+$(document).ready(function(){
+    /** Get the trackers from the Database and then dynamically
+     * render the the return as a dropdown list
+     */
+    function renderTrackers(trackers){
+
+    }
+
+    /** Get the website from the Database and then dynamically
+     * render the the return as a dropdown list
+     */
+    function renderWebsitesVisited(websites){
+
+    }
+});
