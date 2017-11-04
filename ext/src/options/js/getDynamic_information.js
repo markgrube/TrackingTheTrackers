b42c0fe154f1da25562484b6107d71efd2c2e2f2
david-agumya <agumya.d@husky.neu.edu>
2017-11-04 16:42:13 -0700

    Parsing incoming trackers from DB

--- ext/src/options/js/getDynamic_information.js
+++ ext/src/options/js/getDynamic_information.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
     /** Get the trackers from the Database and then dynamically
      * render the the return as a dropdown list
      */
+    var sample_trackers = [ "www.google.com", "www.facebook", "www.amazon.com"]
     function renderTrackers(trackers){
+        trackers.forEach(function(tracker){
+            $("#tracker_info").append("<li>"+"tracker"+"<li>")
+        });
 
     }
 
