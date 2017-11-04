7d1a5a101df56f8bc0cd387d269797bbb6d0b881
Yoganand Chandrasekhar <chandrasekhar.y@husky.neu.edu>
2017-11-04 15:08:09 -0700

    backgeound

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,13 +1,11 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+window.addEventListener('load', function(evt) {
+	console.log(window.blacklist);
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+	chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		chrome.extension.getBackgroundPage().console.log(details.url);
+	}, 
+	{urls: ["<all_urls>"]}, 
+	["blocking"]);
+});
 
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
