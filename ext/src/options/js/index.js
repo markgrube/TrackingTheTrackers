a80ba0e13eb5ba9e0bd07a8420cd0f7ec3edcfe8
Mark Grube <markg140@gmail.com>
2017-11-04 18:04:11 -0700

    added charting

--- ext/src/options/js/index.js
+++ ext/src/options/js/index.js
@@ -1,14 +1,14 @@
 $(function() {
     chrome.storage.local.get("trackers", function(result){
-        trackerNames = [];
-        numWebsites = [];
+        var trackerNames = [];
+        var numWebsites = [];
         if (result["trackers"] !== undefined) {
-            var allTrackers = result["trackers"]
+            var allTrackers = result["trackers"];
 
             for (var key in allTrackers) {
                 if (allTrackers.hasOwnProperty(key)) {
                     trackerNames.push(key);
-                    numWebsites.push(allTrackers[key]);
+                    numWebsites.push(allTrackers[key].length);
                 }
             }
 
@@ -23,7 +23,7 @@ $(function() {
             };
             new Chart({
                 parent: "#trackers-chart",
-                title: "My Awesome Chart",
+                title: "Tracker Chart",
                 data: trackerData,
                 type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
                 height: 250
@@ -31,32 +31,39 @@ $(function() {
 
         }
     });
-});
 
-    // getListOfTrackers(function(result) {
-    //
-    //     var trackerData = {
-    //         labels: result,
-    //
-    //         datasets: [
-    //             {
-    //                 title: "Number of Websites", color: "light-blue",
-    //                 values: [25, 21]
-    //             }
-    //         ]
-    //     };
-    //
-    //     new Chart({
-    //         parent: "#trackers-chart",
-    //         title: "My Awesome Chart",
-    //         data: trackerData,
-    //         type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
-    //         height: 250
-    //     });
-    // });
-    //
-    //
-    //
-    //
+    chrome.storage.local.get("hosts", function(result){
+        var websiteNames = [];
+        var numTrackers = [];
+        if (result["hosts"] !== undefined) {
+            var allTrackers = result["hosts"];
+
+            for (var key in allTrackers) {
+                if (allTrackers.hasOwnProperty(key)) {
+                    websiteNames.push(key);
+                    numTrackers.push(allTrackers[key].length);
+                }
+            }
+
+            var hostData = {
+                labels: websiteNames,
+                datasets: [
+                    {
+                        title: "Number of Trackers", color: "light-blue",
+                        values: numTrackers
+                    }
+                ]
+            };
+            new Chart({
+                parent: "#websites-chart",
+                title: "Website Chart",
+                data: hostData,
+                type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
+                height: 250
+            });
+
+        }
+    });
+});
 
 
