a84cc87d036fdcf1c2c77fb5ada0609dd8be205d
Mark Grube <markg140@gmail.com>
2017-11-04 17:53:04 -0700

    creating charts

--- ext/src/options/js/index.js
+++ ext/src/options/js/index.js
@@ -1,7 +1,62 @@
 $(function() {
+    chrome.storage.local.get("trackers", function(result){
+        trackerNames = [];
+        numWebsites = [];
+        if (result["trackers"] !== undefined) {
+            var allTrackers = result["trackers"]
 
-	// var tracker = new Chart({
-		
-	// });
+            for (var key in allTrackers) {
+                if (allTrackers.hasOwnProperty(key)) {
+                    trackerNames.push(key);
+                    numWebsites.push(allTrackers[key]);
+                }
+            }
 
+            var trackerData = {
+                labels: trackerNames,
+                datasets: [
+                    {
+                        title: "Number of Websites", color: "light-blue",
+                        values: numWebsites
+                    }
+                ]
+            };
+            new Chart({
+                parent: "#trackers-chart",
+                title: "My Awesome Chart",
+                data: trackerData,
+                type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
+                height: 250
+            });
+
+        }
+    });
 });
+
+    // getListOfTrackers(function(result) {
+    //
+    //     var trackerData = {
+    //         labels: result,
+    //
+    //         datasets: [
+    //             {
+    //                 title: "Number of Websites", color: "light-blue",
+    //                 values: [25, 21]
+    //             }
+    //         ]
+    //     };
+    //
+    //     new Chart({
+    //         parent: "#trackers-chart",
+    //         title: "My Awesome Chart",
+    //         data: trackerData,
+    //         type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
+    //         height: 250
+    //     });
+    // });
+    //
+    //
+    //
+    //
+
+
